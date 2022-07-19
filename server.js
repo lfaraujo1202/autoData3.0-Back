82ce727480831f2d1df967412135a99a09884009
Luiz Felipe <luiz.araujo@veredaeducacao.com>
2022-07-19 17:02:27 -0300

    first commit

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
+require('dotenv').config()
+
 const app = require('./src/app')
-const PORT = 3000
+
 // 3000, 6000, 6060, 8080, 8088
 
-app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`))
+// app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`))
+
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
